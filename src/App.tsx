67a9e752230d6953be9af13903abe24376e732f6
Guillermo González Calderín <guillezdev@gmail.com>
2024-07-03 18:02:24 -0400

    add empty state

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import { useState } from "react";
 //Icons-----------------------------------
 import PlaceIcon from "@mui/icons-material/Place";
 import NightsStayIcon from "@mui/icons-material/NightsStay";
+import noContentIcon from "../public/ic-content.svg";
 
 function App() {
   const { clima, getClima, isFetching } = useServerClima();
@@ -66,57 +67,72 @@ function App() {
 
           {isFetching ? (
             <CircularProgress sx={{ marginTop: "20px", alignSelf: "center" }} />
+          ) : clima?.current ? (
+            <Card sx={{ width: "100%", marginTop: "20px" }}>
+              <CardContent>
+                <Typography
+                  variant="body1"
+                  color="primary"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginBottom: "12px",
+                  }}
+                >
+                  <PlaceIcon fontSize="small" color="primary" />{" "}
+                  {clima?.location?.name}, {clima?.location?.region},{" "}
+                  {clima?.location?.country}
+                </Typography>
+                <Typography variant="h5">Condiciones del tiempo</Typography>
+                <Box display="flex" alignItems="center" marginY="12px">
+                  <CardMedia
+                    component="img"
+                    sx={{ width: 64, height: 64 }}
+                    image={clima?.current?.condition?.icon}
+                    alt={clima?.current?.condition?.text}
+                  />
+                  <Typography variant="h5" sx={{ marginLeft: "12px" }}>
+                    {clima?.current?.temp_c} <sup>o</sup>C
+                  </Typography>
+                </Box>
+                <Typography variant="body2" color="text.secondary">
+                  {clima?.current?.condition?.text}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Humedad: {clima?.current?.humidity}%
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Viento: {clima?.current?.wind_kph} kph{" "}
+                  {clima?.current?.wind_dir}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Sensación térmica: {clima?.current?.feelslike_c} <sup>o</sup>C
+                </Typography>
+              </CardContent>
+            </Card>
           ) : (
-            !!clima?.current && (
-              <Card sx={{ width: "100%", marginTop: "20px" }}>
-                <CardContent>
-                  <Typography
-                    variant="body1"
-                    color="primary"
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      marginBottom: "12px",
-                    }}
-                  >
-                    <PlaceIcon fontSize="small" color="primary" />{" "}
-                    {clima?.location?.name}, {clima?.location?.region},{" "}
-                    {clima?.location?.country}
-                  </Typography>
-                  <Typography variant="h5">Condiciones del tiempo</Typography>
-                  <Box display="flex" alignItems="center" marginY="12px">
-                    <CardMedia
-                      component="img"
-                      sx={{ width: 64, height: 64 }}
-                      image={clima?.current?.condition?.icon}
-                      alt={clima?.current?.condition?.text}
-                    />
-                    <Typography variant="h5" sx={{ marginLeft: "12px" }}>
-                      {clima?.current?.temp_c} <sup>o</sup>C
-                    </Typography>
-                  </Box>
-                  <Typography variant="body2" color="text.secondary">
-                    {clima?.current?.condition?.text}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Humedad: {clima?.current?.humidity}%
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Viento: {clima?.current?.wind_kph} kph{" "}
-                    {clima?.current?.wind_dir}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Sensación térmica: {clima?.current?.feelslike_c}{" "}
-                    <sup>o</sup>C
-                  </Typography>
-                </CardContent>
-              </Card>
-            )
+            <Box
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              minHeight={"100px"}
+              width={"100%"}
+              flexDirection={"column"}
+            >
+              <CardMedia
+                component="img"
+                sx={{ width: 260 }}
+                image={noContentIcon}
+                alt={clima?.current?.condition?.text}
+              />
+              <Typography variant="body2" color="#C4CCD3" ml={1}>
+                Sin datos para mostrar
+              </Typography>
+            </Box>
           )}
         </Box>
       </Box>
     </>
   );
 }
-
 export default App;
