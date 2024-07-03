22b415a7198456ada3c5aaace82691e2b5dceb56
Guillermo González Calderín <guillezdev@gmail.com>
2024-07-02 23:25:54 -0400

    style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import { Box, Button, Container } from "@mui/material";
+import { Box, Button, AppBar, Toolbar, Typography } from "@mui/material";
 import "./App.css";
 import { SurfingOutlined } from "@mui/icons-material";
-
+import CountrySelect from "./components/CountrySelect";
+import NightsStayIcon from "@mui/icons-material/NightsStay";
 function App() {
   return (
     <>
-      <Container>
-        <Box display={"flex"}>Clima app</Box>
-        <Button startIcon={<SurfingOutlined />} variant="text">
-          Probar clima
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <NightsStayIcon />
+          <Typography variant="h6">Clima app</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        display={"flex"}
+        justifyContent={"start"}
+        alignItems={"start"}
+        flexDirection={"column"}
+        m={5}
+        maxWidth={400}
+        mx={"auto"}
+        gap={2}
+      >
+        <CountrySelect />
+        <Button startIcon={<SurfingOutlined />} variant="contained">
+          Optener clima
         </Button>
-      </Container>
+      </Box>
     </>
   );
 }
